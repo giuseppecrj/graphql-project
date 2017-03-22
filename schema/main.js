2d935e53f4865d8f5b25891910d9ec8cf0d7a148
Herman-Scheer <g@herman-scheer.com>
2017-03-21 17:38:25 -0700

    initial commit

--- schema/main.js
+++ schema/main.js
@@ -1,4 +1,15 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
 
+const queryType = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
+    }
+  }
+})
+
 export const mySchema = new GraphQLSchema({
+  query: queryType
 })
